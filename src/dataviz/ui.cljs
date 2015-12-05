4f33ddd2c35369ba33b8bb5bba321f5d0fee562e
vladminsky <vlad.minsky@gmail.com>
2015-12-05 20:02:17 +0300

    pass schema keys

--- src/dataviz/ui.cljs
+++ src/dataviz/ui.cljs
@@ -13,51 +13,15 @@
 
 (enable-console-print!)
 
-(q/defcomponent App
-  []
-  (d/div {}
-         (Header)
-         (Content)
-         (Footer)
-  ))
-
 (q/defcomponent Header
   []
   (d/header {:id "header"}
             (d/h1 {} "DataViz")))
 
-(q/defcomponent Content
-  []
-  (d/section {:id "content"}
-             (PanelWizard)
-             (PanelResult)
-  ))
-
 (q/defcomponent Footer
   []
   (d/footer {:id "footer"}
             (d/p {} "(c) TauCrew 2015")))
-
-(q/defcomponent PanelWizard
-  []
-  (d/section {:id "panel-wizard"}
-             (d/div {}
-                    "Here you will be able to select "
-                    (Selector {
-                                :items ["A", "B", "C"]
-                                :value "B"
-                                :onChange #(println (str "X was changed to " %1))
-                              })
-                    " as X"
-                    " and "
-                    (Selector {
-                                :items ["1", "2", "3"]
-                                :value "2"
-                                :onChange #(println (str "Y was changed to " %1))
-                              })
-                    " as Y"
-             )
-  ))
 
 (q/defcomponent Option
   [item is-selected?]
@@ -83,7 +47,26 @@
   )
 )
 
-
+(q/defcomponent PanelWizard
+  [full-state]
+  (d/section {:id "panel-wizard"}
+             (d/div {}
+                    "Here you will be able to select "
+                    (Selector {
+                                :items (:xs full-state)
+                                :value (:x? full-state)
+                                :onChange #(println (str "X was changed to " %1))
+                              })
+                    " as X"
+                    " and "
+                    (Selector {
+                                :items (:ys full-state)
+                                :value (:y? full-state)
+                                :onChange #(println (str "Y was changed to " %1))
+                              })
+                    " as Y"
+             )
+  ))
 
 (defn gen-table
   "Generate `size` rows vector of 4 columns vectors to mock up the table."
@@ -120,6 +103,33 @@
              )
   ))
 
+(q/defcomponent Content
+  [full-state]
+  (d/section {:id "content"}
+             (PanelWizard full-state)
+             (PanelResult)
+  ))
+
+(q/defcomponent App
+  [full-state]
+  (d/div {}
+         (Header)
+         (Content full-state)
+         (Footer)
+  ))
+
+(defn app-state
+  [schema]
+
+    (def schema-keys (map name schema))
+    {
+       :xs schema-keys
+       :ys schema-keys
+       :x? (last schema-keys)
+       :y? (last schema-keys)
+    })
+
 (defn render
-  []
-  (q/render (App) js/document.body))
+  [schema slice-interceptor]
+;;(println slice-interceptor)
+  (q/render (App (app-state schema)) js/document.body))
