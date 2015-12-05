0a1721cf93c220edcb34aef0bdc815bfade4a6da
vladminsky <vlad.minsky@gmail.com>
2015-12-05 13:27:18 +0300

    Draft vision

--- src/dataviz/ui.cljs
+++ src/dataviz/ui.cljs
@@ -6,18 +6,43 @@
 
 (enable-console-print!)
 
-(q/defcomponent Header 
-  []
-  (d/header {:id "header"}
-            (d/h1 {} "DataViz")))
-
 (q/defcomponent App
   []
   (d/div {}
          (Header)
-         ))
+         (Content)
+         (Footer)
+  ))
 
-(defn render 
+(q/defcomponent Header
   []
-    (q/render (App) js/document.body)
-  )
+  (d/header {:id "header"}
+            (d/h1 {} "DataViz")))
+
+(q/defcomponent Content
+  []
+  (d/section {:id "content"}
+             (PanelWizard)
+             (PanelResult)
+  ))
+
+(q/defcomponent Footer
+  []
+  (d/footer {:id "footer"}
+            (d/p {} "(c) TauCrew 2015")))
+
+(q/defcomponent PanelWizard
+  []
+  (d/section {:id "content"}
+             (d/div {} "Here you will be able to select X Y and items")
+  ))
+
+(q/defcomponent PanelResult
+  []
+  (d/section {:id "content"}
+             (d/div {} "Here will be a result")
+  ))
+
+(defn render
+  []
+  (q/render (App) js/document.body))
