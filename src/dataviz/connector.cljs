59ebe14301e664d5124321281b1835a33675133f
Andrew Shotkin <shotkin@targetprocess.com>
2015-12-05 15:13:39 +0300

    fetch issue data from git hub

--- src/dataviz/connector.cljs
+++ src/dataviz/connector.cljs
@@ -1,8 +1,31 @@
 (ns dataviz.connector
-  (:require
-    [datascript.core :as data]))
+  (:require-macros [cljs.core.async.macros :refer [go]])
+  (:require [datascript.core :as data]
+  			[cljs-http.client :as http]
+  			[cljs.core.async :refer [<!]]
+  )
+)
+
+(defrecord IssueData [title state])
 
 (defn import 
   []
-    (println "Hello world!")
+  (load-from-outside "https://api.github.com/repos/TargetProcess/tauCharts/issues")
+)
+
+(defn load-from-outside
+  [url]
+  (prn "going to fetch from" url)
+  (go (let [response (<! (http/get url {:with-credentials? false}))]
+  	      (prn "fetched from" url)
+	      (prn "status code" (:status response))
+	      (def data (map (fn[x] (IssueData. (:title x) (:state x))) (:body response)))
+	      (prn "issues" data)
+	      (store data)
+       )
   )
+)
+
+(defn store
+  [data]
+)
